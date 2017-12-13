0e14a982b59d3cd9c45a97b01cd801ce58afa03c
Waldo Lavaut <w86compositor@gmail.com>
2017-12-12 20:12:55 -0500

    Jquery specific rules added

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-    $("#gadgets").click(function(){
-            $("#categories").on("load", "partials/gadgets_partial.html");
-     
-    });
+ $("#gadgets").click(function () {
+     $("#gadgets-categories").show()
+ })
 });
